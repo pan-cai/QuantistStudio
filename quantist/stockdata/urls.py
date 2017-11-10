9f0c2e922610e10bd19b90de018ba092f6ce1ca2
pan-cai <liupan8910@163.com>
2017-11-10 23:25:08 +0800

    0.0.5

--- quantist/stockdata/urls.py
+++ quantist/stockdata/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     url(r'^index/', views.index),
-    url(r'^test_table',views.test_table)
+    url(r'^test_jquery/', views.test_jquery),
+    url(r'^test_table/',views.test_table),
+    url(r'^test_table2/',views.test_table2),
+    url(r'^test_json_ajax/',views.test_json_ajax),
+    url(r'^ajax_list/',views.ajax_list),
+    url(r'^ajax_dict/',views.ajax_dict),
+    url(r'^indicator/',views.indicator),
+
+
 ]
